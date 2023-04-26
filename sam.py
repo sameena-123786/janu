3b79009c0c4594ca8a34aaab3969661d69630c97
sameena-123786 <131848013+sameena-123786@users.noreply.github.com>
2023-04-26 09:19:14 -0700

    Add files via upload

---
+++ sam.py
@@ -0,0 +1,89 @@
+print("hellow world")
+a=10
+print(a)
+a,b,c=5,6,7
+print(a,b,c)
+a=4
+b=2
+x=4
+print(type(x))
+x = 5
+y = 3
+
+print(x + y)
+x = 5
+y = 3
+
+print(x - y)
+x = 5
+y = 3
+
+print(x * y)
+x = 5
+y = 3
+
+print(x / y)
+x = 5
+y = 3
+
+print(x % y)
+x = 5
+y = 3
+
+print(x ** y)
+x = 5
+y = 3
+
+print(x // y)
+x = 5
+
+x += 3
+
+print(x)
+x = 5
+y = 3
+
+print(x > y)
+x = 5
+y = 7
+
+print(x <= y)
+x = 5
+
+print(x > 3 and x < 10)
+fruits = ["apple","banana","orange"]
+print(fruits)
+fruits.append("cherry")
+print(fruits)
+fruits = ["apple","banana","orange"]
+veggies=["tomato","potato","brinjal"]
+fruits.extend(veggies)
+veggies.sort()
+print(veggies)
+print("hellow")
+print('hello')
+a="""sameena is a science student but she is learning now computer"""
+print(a)
+a = "Hello, World!"
+print(a[1])
+for x in "banana":
+  print(x) 
+txt="iam sameena from mahabubnagar!"
+print("sameena" in txt)
+b = "Hello, World!"
+print(b[2:5])
+b = "Hello, World!"
+print(b[:5])
+b = "Hello, World!"
+print(b[-5:-2])
+b = "Hello, World!"
+print(b[2:])
+def my_function(fname):
+  print(fname + " fruit")
+
+my_function("apple")
+my_function("banana")
+my_function("grapes")
+
+
+
